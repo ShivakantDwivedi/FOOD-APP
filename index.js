4eb92e5a7093cf55fd96c879e184a5bd86a34135
ShivakantDwivedi <itshivakant1472002@gmail.com>
2025-02-15 19:39:25 +0530

    image upload api complete

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import connectdb from "./config/db.js";
 import bodyParser from "body-parser";
 import userRoutes from "./route/authRoute.js";
 import productRoute from "./route/productRoute.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -13,6 +15,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(bodyParser.json());
 app.use(express.json());
 
--- middleware/imageUplode.js
+++ middleware/imageUplode.js
@@ -1,8 +1,15 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    const uploadPath = req.body.uploadPath || "uploads";
+
+    // Ensure the directory exists
+    fs.mkdirSync(uploadPath, { recursive: true });
+
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
@@ -10,4 +17,5 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
 export default upload;
